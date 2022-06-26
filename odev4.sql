f7f44068a5cd66ca87e35d155a50a81ed1c98d56
emin-akdemir <108229954+emin-akdemir@users.noreply.github.com>
2022-06-26 18:37:41 +0300

    Create odev4.sql

---
+++ odev4.sql
@@ -0,0 +1,13 @@
+--4
+-- SELECT distinct replacement_cost  from film
+
+-- SELECT count(distinct replacement_cost) from film
+
+-- SELECT count(*) from film 
+-- where title like 'T%' and rating = 'G'
+
+-- SELECT count(*) from country 
+-- where country like '_____';
+
+-- SELECT * from city  
+-- where city  ilike '%R';

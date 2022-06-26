11754f622e18411055863440d12b9dbd0258dc13
emin-akdemir <108229954+emin-akdemir@users.noreply.github.com>
2022-06-26 18:37:10 +0300

    Create odev3.sql

---
+++ odev3.sql
@@ -0,0 +1,12 @@
+--3
+--SELECT * from country
+--where country like 'A%a';
+
+--SELECT * from country
+--where country like '%_____n';
+
+--SELECT title  from film
+--where title ilike '%T%T%T%T%';
+
+--SELECT *  from film
+--where title ilike 'C%' and length > 90 and rental_rate = 2.99;

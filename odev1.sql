70096c61d40207fb3a50ed170d14b640c8e849f1
emin-akdemir <108229954+emin-akdemir@users.noreply.github.com>
2022-06-26 18:32:36 +0300

    Create odev1.sql

---
+++ odev1.sql
@@ -0,0 +1,13 @@
+-- 1
+-- SELECT title, description from film;
+
+-- SELECT * from film where length >60 and length < 75;
+
+-- SELECT * from film 
+-- where rental_rate = 0.99 and replacement_cost = 12.99 or replacement_cost = 28.99;
+
+-- SELECT first_name, last_name from customer
+-- where first_name = 'Mary'
+
+-- SELECT * from film
+-- where not (length > 50 and rental_rate = 2.99 or rental_rate = 4.99);

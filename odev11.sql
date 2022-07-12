9a647d7b9a2603814bf3eb8b9c8f4072d64482b7
emin-akdemir <108229954+emin-akdemir@users.noreply.github.com>
2022-07-12 22:02:21 +0300

    Create odev11.sql

---
+++ odev11.sql
@@ -0,0 +1,9 @@
+--9
+--select city.city, country.country from country
+--inner join city on city.city_id = country.country_id;
+
+--select customer.first_name , customer.last_name, payment.payment_id from customer 
+--inner join payment on payment.customer_id = customer.customer_id;
+
+--select customer.first_name , customer.last_name, rental.rental_id from rental 
+--inner join customer on rental.customer_id = customer.customer_id;
